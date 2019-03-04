0bf41592088e22735f9df427201b9b895cac6330
倉田 和也 <btkuratakz@magnes-s.local>
2019-03-04 19:18:41 +0900

    make sample app

--- src/main/java/com/zomu/t/epion/tropic/test/tool/epiont3targetapprest/app/controller/TodosController.java
+++ src/main/java/com/zomu/t/epion/tropic/test/tool/epiont3targetapprest/app/controller/TodosController.java
@@ -1,28 +1,85 @@
 package com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.app.controller;
 
-import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.app.model.Todo;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.app.model.Todo;
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.domain.service.TodoCrudService;
+
+import lombok.extern.slf4j.Slf4j;
 
 
 @RestController
 @RequestMapping(value = "todos")
 @Slf4j
 public class TodosController {
+	
+	@Autowired
+	TodoCrudService todoCrudService;
 
     @PostMapping
     public ResponseEntity<Todo> create(
             @RequestBody @Validated Todo todo) {
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    	
+    	Todo createTodo = todoCrudService.create(todo);
+    	
+        return new ResponseEntity<>(createTodo, HttpStatus.CREATED);
     }
 
+    @PutMapping
+    public ResponseEntity<Todo> update(
+            @RequestBody @Validated Todo todo) {
+    	
+    	Todo updateTodo = todoCrudService.update(todo);
+    	
+    	if (updateTodo == null) {
+    		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    	}
+    	
+        return new ResponseEntity<>(updateTodo, HttpStatus.OK);
+    }
+    
+    @GetMapping("refer")
+    public ResponseEntity<Todo> read(
+    		@RequestParam String todoId) {
+    	
+    	Todo todo = todoCrudService.refer(todoId);
+    	
+        return new ResponseEntity<>(todo, HttpStatus.OK);
+    }
+    
+    @GetMapping("search")
+    public ResponseEntity<List<Todo>> search(
+    		@RequestParam String key, @RequestParam String value) {
+    	
+    	List<Todo> todoList = todoCrudService.search(key, value);
+    	
+        return new ResponseEntity<>(todoList, HttpStatus.OK);
+    }
+    
+    @DeleteMapping
+    public ResponseEntity<Todo> delete(
+            @RequestParam String todoId) {
+    	
+    	Todo deleteTodo = todoCrudService.delete(todoId);
+    	
+    	if (deleteTodo == null) {
+    		return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    	}
+    	
+        return new ResponseEntity<>(deleteTodo, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/zomu/t/epion/tropic/test/tool/epiont3targetapprest/domain/service/TodoCrudService.java
+++ src/main/java/com/zomu/t/epion/tropic/test/tool/epiont3targetapprest/domain/service/TodoCrudService.java
@@ -1,15 +1,206 @@
 package com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.domain.service;
 
 import lombok.extern.slf4j.Slf4j;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.app.model.Todo;
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.domain.mapper.TodoMapper;
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.domain.mapper.TodoModelMapper;
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.domain.model.TodoModel;
+import com.zomu.t.epion.tropic.test.tool.epiont3targetapprest.domain.model.TodoModelExample;
 
 @Service
+@Transactional
 @Slf4j
 public class TodoCrudService {
+	
+	@Autowired
+	TodoModelMapper todoModelMapper;
+	
+	@Autowired
+	TodoMapper todoMapper;
+    
+    /**
+     * Todoの作成を行う.
+     * 
+     * @param todo
+     */
+    public Todo create(Todo todo) {
+		
+    	if (todo == null) {
+    		return null;
+    	}
 
+    	String id = UUID.randomUUID().toString();
+    	
+    	todo.setId(id);
+    	
+    	TodoModel todoModel = changeToModel(todo);
+    	
+    	todoModelMapper.insert(todoModel);
+    	
+    	return todo;
+    }
+    
+    /**
+     * Todoの更新を行う.
+     * 
+     * @param todo
+     */
+    public Todo update(Todo todo) {
+		
+    	if (todo == null) {
+    		return null;
+    	}
 
-    public Todo refer(String id) {
-
+    	TodoModel todoModel = changeToModel(todo);
+    	
+    	return (todoModelMapper.updateByPrimaryKey(todoModel) == 0) ? null : changeFromModel(todoModel);
+    }
+    
+    /**
+     * Todoの削除を行う.
+     * 
+     * @param todo
+     */
+    public Todo delete(String todoId) {
+		
+    	if (todoId == null) {
+    		return null;
+    	}
+    	
+    	TodoModel todoModel = todoModelMapper.selectByPrimaryKey(todoId);
+    	
+    	if (todoModel == null) {
+    		return null;
+    	}
+    	
+    	return (todoModelMapper.deleteByPrimaryKey(todoId) == 0) ? null : changeFromModel(todoModel);
+    }
+    
+    /**
+     * Todoの取得(1件)を行う.
+     * 
+     * @param todo
+     */
+    public Todo refer(String todoId) {
+		
+    	if (todoId == null) {
+    		return null;
+    	}
+    	
+    	TodoModel todoModel = todoModelMapper.selectByPrimaryKey(todoId);
+    	
+    	if (todoModel == null) {
+    		return null;
+    	}
+    	
+    	return changeFromModel(todoModel);
+    }
+    
+    public List<Todo> search(String key, String value) {
+    	
+    	if (key == null) {
+    		return null;
+    	}
+    	
+    	List<Todo> todoList = new ArrayList<>();
+    	
+    	if (!key.equals("tags")) {
+    		List<TodoModel> todoModelList = todoMapper.selectKeyValue(key, value);
+    		for (TodoModel todoModel : todoModelList) {
+    			todoList.add(changeFromModel(todoModel));
+    		}
+    	} else {
+    		List<TodoModel> todoModelList = todoMapper.selectAll();
+    		for (TodoModel todoModel : todoModelList) {
+    			if (todoModel.getTags() != null) {
+    				if (Arrays.asList(todoModel.getTags().split(",")).contains(value)) {
+    					todoList.add(changeFromModel(todoModel));
+    				}
+    			}
+    			
+    		}
+    	}
+    	
+    	return todoList;
+    }
+    
+    private TodoModel changeToModel(Todo todo) {
+    	// DB登録用のモデル生成
+    	TodoModel todoModel = new TodoModel();
+    	
+    	todoModel.setId(todo.getId());
+    	todoModel.setTitle(todo.getTitle());
+    	todoModel.setDescription(todo.getDescription());
+    	
+    	if (todo.getStart() != null) {
+    		todoModel.setStart(Date.from(todo.getStart().toInstant()));
+    	}
+    	
+    	if (todo.getDue() != null) {
+    		todoModel.setDue(Date.from(todo.getDue().toInstant()));
+    	}
+    	
+    	if (todo.getActualStart() != null) {
+    		todoModel.setActualstart(Date.from(todo.getActualStart().toInstant()));
+    	}
+    	
+    	if (todo.getActualEnd() != null) {
+    		todoModel.setActualend(Date.from(todo.getActualEnd().toInstant()));
+    	}
+    	
+    	todoModel.setStatus(todo.getStatus());
+    	todoModel.setPriority(todo.getPriority());
+    	
+    	if (todo.getTags() != null) {
+    		todoModel.setTags(String.join(",", todo.getTags()));
+    	}
+    	
+    	return todoModel;
+    }
+    
+    private Todo changeFromModel(TodoModel todoModel) {
+    	Todo todo = new Todo();
+    	
+    	todo.setId(todoModel.getId());
+    	todo.setTitle(todoModel.getTitle());
+    	todo.setDescription(todoModel.getDescription());
+    	
+    	if (todoModel.getStart() != null) {
+    		todo.setStart(OffsetDateTime.ofInstant(todoModel.getStart().toInstant(), ZoneId.systemDefault()));
+    	}
+    	
+    	if (todoModel.getDue() != null) {
+    		todo.setDue(OffsetDateTime.ofInstant(todoModel.getDue().toInstant(), ZoneId.systemDefault()));
+    	}
+    	
+    	if (todoModel.getActualstart() != null) {
+    		todo.setActualStart(OffsetDateTime.ofInstant(todoModel.getActualstart().toInstant(), ZoneId.systemDefault()));
+    	}
+    	
+    	if (todoModel.getActualend() != null) {
+    		todo.setActualEnd(OffsetDateTime.ofInstant(todoModel.getActualend().toInstant(), ZoneId.systemDefault()));
+    	}
+    	
+    	todo.setStatus(todoModel.getStatus());
+    	todo.setPriority(todoModel.getPriority());
+    	
+    	if (todoModel.getTags() != null) {
+    	    todo.setTags(Arrays.asList(todoModel.getTags().split(",")));
+        }
+    	return todo;
     }
 
 }
